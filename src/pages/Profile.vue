4a00946fbc962bac78f7b9eafb5c2e698a5e6a90
ruanvalente <ruanopensuselinux@gmail.com>
2021-02-11 22:17:41 -0300

    wip: added style profile image and username

--- src/pages/Profile.vue
+++ src/pages/Profile.vue
@@ -1,6 +1,13 @@
 <template>
-  <div>
-    <h2>Profile</h2>
+  <div class="container">
+    <div>
+      <header>
+        <img src="@/assets/default-image.jpg" alt="Profile image" />
+        <div>
+          <strong>User name</strong>
+        </div>
+      </header>
+    </div>
   </div>
 </template>
 
@@ -12,4 +19,42 @@ export default defineComponent({})
 
 <style lang="scss" scoped>
 
+.container {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  div {
+    display: flex;
+    flex-direction: column;
+    place-content: center;
+    width: 100%;
+    max-width: 700px;
+
+    header {
+      display: flex;
+      align-items: center;
+
+      img {
+        width: 60px;
+        height: 60px;
+        border-radius: 50%;
+      }
+
+      div {
+        margin-left: 30px;
+
+        strong {
+          font-size: 1.5rem;
+          line-height: 2.625rem;
+          color: #263238;
+
+          @media(max-width: 600px) {
+            font-size: 1rem;
+          }
+        }
+      }
+    }
+  }
+}
 </style>
